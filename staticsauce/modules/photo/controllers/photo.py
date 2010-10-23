095d64dc0ef512db46ba3bed19729d8c367ae877
Jon Dufresne <jon.dufresne@gmail.com>
2010-10-22 19:16:06 -0700

    fix photo module to work with templating changes

--- staticsauce/modules/photo/controllers/photo.py
+++ staticsauce/modules/photo/controllers/photo.py
@@ -16,39 +16,35 @@
 
 import datetime
 from staticsauce.controller import Controller
-from staticsauce import templating
+from staticsauce.templating import render
 from staticsauce.modules.photo import models
 from staticsauce.helpers import *
 
+
 class PhotoController(Controller):
     def albums(self):
-        render = templating.render_jinja2
-        return render('/photo/albums.html', albums=models.albums())
+        return render('/photo/albums.html', {'albums': models.albums()})
 
     def album(self, slug):
-        render = templating.render_jinja2
         album = models.album(slug)
-        return render('/photo/album.html', album=album)
+        return render('/photo/album.html', {'album': album})
 
     def photo(self, album_slug, slug):
-        render = templating.render_jinja2
         album = models.album(album_slug)
         photo = album.photo(slug)
-        return render('/photo/photo.html', photo=photo)
+        return render('/photo/photo.html', {'photo': photo})
 
     def feed(self):
-        render = templating.render_jinja2
         albums = albums=models.albums()
         most_recent = min(albums, key=lambda x: x.date) \
             if albums else datetime.datetime.now()
         updated = most_recent.date
-        return render(
-            '/photo/feed.xml',
-            atom_uuid=atom_uuid,
-            rfc3339=rfc3339,
-            updated=updated,
-            albums=albums
-        )
+        return render('/photo/feed.xml', {
+            'atom_uuid': atom_uuid,
+            'rfc3339': rfc3339,
+            'updated': updated,
+            'albums': albums,
+        })
 
 
 __controller__ = PhotoController
