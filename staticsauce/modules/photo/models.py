debfebdb054339eba7e1ac64fe6b68c957482d9b
Jon Dufresne <jon.dufresne@gmail.com>
2010-10-31 22:48:47 -0700

    restructed models as python objects

--- staticsauce/modules/photo/models.py
+++ staticsauce/modules/photo/models.py
@@ -14,83 +14,46 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import os
-import datetime
-import xml.dom.minidom
 from staticsauce import config
-from staticsauce.utils import get_element_text, slug_from_filename
-
-
-def parse_album(filename):
-    albums_dir = os.path.join(config.get('project', 'data_dir'),
-                              'photo', 'albums')
-    document = xml.dom.minidom.parse(os.path.join(albums_dir, filename))
-    slug = slug_from_filename(filename)
-    title = get_element_text(document, 'title')
-    year, month, day = map(int, get_element_text(document, 'date').split('-'))
-    date = datetime.date(year, month, day)
-    try:
-        description = get_element_text(document, 'description')
-    except KeyError:
-        description = None
-    album = Album(slug, title, date, description)
-    cover = get_element_text(document, 'cover')
-    return album, cover
-
-
-def photos(album, cover_filename):
-    photos = []
-    cover = None
-    images_dir = os.path.join(config.get('project', 'data_dir'),
-                              'photo', 'images', album.slug)
-
-    for index, filename in enumerate(sorted(os.listdir(images_dir))):
-        slug = '{album}{index}'.format(album=album.slug, index=index)
-        photo = Photo(slug, album)
-
-        if cover_filename == filename:
-            cover = photo
-
-        photos.append(photo)
-
-    return photos, cover
+from staticsauce.utils import import_path
 
 
 def albums():
-    albums = []
-    albums_dir = os.path.join(config.get('project', 'data_dir'),
-                              'photo', 'albums')
-
-    for filename in os.listdir(albums_dir):
-        album, cover = parse_album(filename)
-        album.photos, album.cover = photos(album, cover)
-        albums.append(album)
-    return sorted(albums, key=lambda album: album.date, reverse=True)
+    module = import_path('data.photo')
+    return module.albums
 
 
 def album(slug):
-    album, cover = parse_album('.'.join([slug, 'xml']))
-    album.photos, album.cover = photos(album, cover)
-    return album
+    for album in albums():
+        if album.slug == slug:
+          return album
+    raise KeyError
 
 
-class Album:
-    def __init__(self, slug, title, date, description):
+class Album(object):
+    def __init__(self, slug, title, date, description, photos):
         self.slug = slug
         self.title = title
         self.date = date
         self.description = description
+        self.photos = photos
         self.cover = None
-        self.photos = None
 
-    def add_photo(self, photo):
-        photo.album = self
-        self.photos.append(photo)
+        for photo in photos:
+            photo.album = self
+            if photo.cover:
+                self.cover = photo
 
     def images(self):
         url = '{site_root}/images/photo/{slug}'
-        return url.format(site_root=config.get('site', 'site_root'),
-                          slug=self.slug)
+        return url.format(
+            site_root=config.get('site', 'site_root'),
+            slug=self.slug
+        )
+
+    def thumbnails(self):
+        url = '{images}/thumbnails'
+        return url.format(images=self.images())
 
     def photo(self, photo_slug):
         for photo in self.photos:
@@ -99,18 +62,21 @@ class Album:
         raise KeyError
 
 
-class Photo:
-    def __init__(self, slug, album):
+class Photo(object):
+    def __init__(self, slug, filename, description=None, cover=False):
         self.slug = slug
-        self.album = album
-
-    def thumbnail(self):
-        url = '{images}/thumbnails/{slug}.jpeg'
-        return url.format(images=self.album.images(), slug=self.slug)
+        self.filename = filename
+        self.description = description
+        self.cover = cover
+        self.album = None
 
     def image(self):
         url = '{images}/{slug}.jpeg'
         return url.format(images=self.album.images(), slug=self.slug)
+
+    def thumbnail(self):
+        url = '{thumbnails}/{slug}.jpeg'
+        return url.format(thumbnails=self.album.thumbnails(), slug=self.slug)
 
     def prev(self):
         index = self.album.photos.index(self)
--- staticsauce/utils.py
+++ staticsauce/utils.py
@@ -34,18 +34,6 @@ def path_append(root, path):
     return path
 
 
-def slug_from_filename(filename):
-    return os.path.splitext(os.path.basename(filename))[0]
-
-
-def get_element_text(document, name):
-    nodes = document.getElementsByTagName(name)
-    if len(nodes) != 1:
-        raise KeyError(name)
-    node = nodes[0]
-    return ''.join([child_node.toxml() for child_node in node.childNodes])
-
-
 def rfc3339(datetime):
     return datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
 
