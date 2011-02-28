f9b1b776e58f5626411932e917aba001a712ed35
Jon Dufresne <jon.dufresne@gmail.com>
2011-02-27 17:08:03 -0800

    remove unused import

--- staticsauce/feed.py
+++ staticsauce/feed.py
@@ -14,7 +14,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import time
 from xml.etree import ElementTree
 from staticsauce.conf import settings
 from staticsauce.templating import render
